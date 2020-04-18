fd6e62f031031a5171b213c81d50e6589938377f
empty <zshgrlai@163.com>
2020-04-18 10:49:27 +0800

    .

--- 6.vue_demo/components/Parent.vue
+++ 6.vue_demo/components/Parent.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
         父亲{{money}}
-        <Son1  :sonMoney='money'> </Son1>
+        <!-- <Son1  :sonMoney='money'  @update="change"> </Son1> -->
+        <!-- .sync 的写法  -->
+        <!-- <Son1 :sonMny.sync="money"></Son1> -->
+        <Son1 v-model="money"></Son1>
+
+        <!-- v-model 写法 ，自组件只能更改value,因为他是 , <input :value  :input="xxx" >的语法糖   -->
     </div>
 </template>
 
@@ -15,6 +20,9 @@ export default {
         return {
             money:200
         }
-    }
+    },
+   methods:{
+     
+   }
 }
 </script>
--- 6.vue_demo/components/Son1.vue
+++ 6.vue_demo/components/Son1.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        儿子 收到 {{sonMoney}}
+        儿子 收到 {{value}}
+        <button  @click="changeMny"  >点击修改</button>
     </div>
 </template>
 
@@ -12,9 +13,17 @@ export default {
         }
     },
     props:{
-        sonMoney:{
+        value:{
             type:Number,
             default:0
+        }
+    },
+    methods:{
+        changeMny(){
+            // console.log('增加mny=400')
+            // this.$emit('update:value',400)
+
+            this.$emit('input',300)
         }
     }
 }
