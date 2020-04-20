fb7e8d131078b3ff0eca1d70d8a446c4010499cf
empty <zshgrlai@163.com>
2020-04-20 15:34:16 +0800

    .

--- testDemo/1.a==1_a==2true.js
+++ testDemo/1.a==1_a==2true.js
@@ -1,3 +1,21 @@
-let a = '11';
+// let a = '11';
 
-console.log(a == 1 && a == 2);
+// console.log(a == 1 && a == 2);
+
+var A= function(){ this.value =1};
+A.prototype.value=2;
+var a = new A ();
+var b = {value :4};
+var B = A.bind(b);
+var c = {value:5};
+var C = B.bind(c);
+var value =3;
+
+A();
+B();
+C();
+
+console.log(value);
+console.log(a.value);
+console.log(b.value);
+console.log(c.value);
