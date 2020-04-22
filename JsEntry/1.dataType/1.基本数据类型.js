afa24ee066d82b67de09216798aaa40bbc6af3e9
empty <zshgrlai@163.com>
2020-04-22 22:51:58 +0800

    .

--- JsEntry/1.dataType/1.基本数据类型.js
+++ JsEntry/1.dataType/1.基本数据类型.js
@@ -10,43 +10,47 @@
 //检查数据类型的四种方式
 //  typeof  instanceOf  constructor Object.prototype.toString.call()
 
-//1、 typeof 可以检测 number/String/ boolean；但是无法检测出null,object,Array
+//1、 typeof 可以检测 number/String/ boolean/undefined/function；但是无法检测出null,object,Array
 
 console.log(typeof 10);    // 'number'
 console.log(typeof '19');   //'String'
 console.log(typeof false);       //'boolean'
 console.log(typeof null);   // 'object'
+console.log(typeof undefined);   // 'undefined'
 console.log(typeof {name: 'jack'});  //'object'
 console.log(typeof [10, 20]);    //'object'
-console.log(typeof new Date());  //'object'
+console.log(typeof function(){});  //'function'
 console.log(typeof Symbol(10));   //symbol
-// 2、检测null,两种方法
-let obj=null;
-// !obj 是真的，由三种情况，null,undefined,0
-if(!obj && typeof obj !=='undefined'&& obj !==0){
-      console.log('=====obj是null=====111');
-}
-if(obj===null){
-      console.log('=====obj是null=====222');
-}
+// // 2、检测null,undefined
+// let obj=null;
+// // null,undefinde 都严格等于自己，非严格相等
+// if(undefined === undefined){
+//       console.log('=====undefined=====111');
+// }
+// if(obj===null){
+//       console.log('=====obj是null=====222');
+// }
+// if(null==undefined){
+//       console.log('========null=undefined===========')
+// }
 
-//3、检测数组  有3种方法
-let arr=[10,20];
+// //3、检测数组  有4种方法
+// let arr=[10,20];
 
-if(arr instanceof Array){
-      console.log('arr是数组类型11111',arr instanceof Array);
-}
+// if(arr instanceof Array){
+//       console.log('arr是数组类型11111',arr instanceof Array);
+// }
 
-if(arr.constructor===Array){
-      console.log('arr是数组类型22222',arr.constructor);
-}
+// if(arr.constructor===Array){
+//       console.log('arr是数组类型22222',arr.constructor);
+// }
 
-if(Array.isArray(arr)){
-      console.log('arr是数组类型33333',Array.isArray(arr));
-}
+// if(Array.isArray(arr)){
+//       console.log('arr是数组类型33333',Array.isArray(arr));
+// }
 
 
-if(Object.prototype.toString.call(arr)==="[object Array]"){
-      console.log('arr是数组类型444444',Object.prototype.toString.call(arr));
-}
+// if(Object.prototype.toString.call(arr)==="[object Array]"){
+//       console.log('arr是数组类型444444',Object.prototype.toString.call(arr));
+// }
 
--- JsEntry/1.dataType/2.深浅拷贝.js
+++ JsEntry/1.dataType/2.深浅拷贝.js
@@ -1,58 +1,40 @@
 /*  拷贝分为：深拷贝、浅拷贝
-*  浅拷贝：
-*  深拷贝：
-* */
-let person={
-      name:undefined,
-      score:{
-          english:80,
-          math:100
-      },
-      animal:[{name:'dog',num:2},{name:'cat',num:1}]
+ *  浅拷贝：
+ *  深拷贝：
+ * */
+let person = {
+  name: "jack",
+  age: null,
+  height: undefined,
+  color: {a:1,b:{c:22}},
+  address: Symbol("new york"),
+  speak: function () {
+    console.log("english");
+  },
+  time: new Date(),
 };
 
-Object.defineProperty(person,'age',{
-      enumerable: true,
-      value: 8
-});
+//1. JSON.parse(JSON.stringify(person))   丢失 undefined,Symbol,function 等类型
 
+// console.log(JSON.parse(JSON.stringify(person))) //{ name: 'jack', age: null, color: {} }
 
-let show = function show(){
-    let a=1;
-    return function () {
-          let b=2;
-      //     console.log(a+b);
-    }
+//2. 实现一个 递归拷贝
+let deepClone = function deepClone(obj) {
+  if (obj == null) return obj;
+  if (obj instanceof Date) return new Date(obj);
+  if (obj instanceof RegExp) return new RegExp(obj);
+  //如果是其他对象，则拷贝每一项；如果不是对象，就返回。
+  if (typeof obj !== "object") return obj;
+  let newObj= new obj.constructor;
+  for(let key in obj){
+        if(obj.hasOwnProperty(key)){
+              newObj[key]=deepClone(obj[key])
+        }
+  }    
+  return newObj
 };
 
-
-let deepClone= function deepClone(obj) {
-      let result=null;
-
-      //如果当前元素是数组
-      if(Object.prototype.toString.call(obj)=='[object Array]'){
-            result=[];
-            for (let i=0;i<obj.length;i++){
-                  result[i]=deepClone(obj[i]);
-            }
-         //如果当前元素是对象
-      }else if(Object.prototype.toString.call(obj)=='[object object]'){
-            result = {};
-            for(var item in obj){
-                 result[item]=deepClone(obj[item]);
-            }
-         //值类型直接返回
-      }else if(Object.prototype.toString.call(obj)=='[Object function]'){
-            return new Function("return " + obj.toString())();
-      }
-      else {
-           return obj;
-      }
-
-      return result;
-};
-
-let p= deepClone(show)
-console.log(p,'show');
-
-// console.log(JSON.parse(JSON.stringify(person)));
+let p = deepClone(person);
+p.color.b.c=99;
+console.log(p);
+console.log(person);
