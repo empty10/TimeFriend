921fd0e4c227e029783ce0b5d88534c233d54c4c
empty <zshgrlai@163.com>
2020-04-23 22:11:47 +0800

    .

--- JsEntry/4. asynchronous/js/4.emit.js
+++ JsEntry/4. asynchronous/js/4.emit.js
@@ -1,25 +1,24 @@
-
 // 给一个事件订阅几个方法，当事件触发的时候依次执行这几个方法
 function Event() {
-      this.event = {};
+  this.event = {};
+}
+Event.prototype.on = function (type, callBack) {
+  if (this.event[type]) {
+    this.event[type].push(callBack);
+  } else {
+    this.event[type] = [callBack];
   }
-  Event.prototype.on = function (type,callBack) {
-      if(this.event[type]){
-          this.event[type].push(callBack);
-      }else{
-          this.event[type] = [callBack];
-      }
-  };
-  Event.prototype.emit = function (type,...data) {
-      this.event[type].forEach((item)=>item(...data));
-  };
-  let event = new Event();
-  function fn1(){
-     console.log('吃饭');
-  }
-  function fn2(){
-      console.log('工作');
-  }
-  event.on('我的一天',fn1);
-  event.on('我的一天',fn2);
-  event.emit('我的一天');
+};
+Event.prototype.emit = function (type, ...data) {
+  this.event[type].forEach((item) => item(...data));
+};
+let event = new Event();
+function fn1() {
+  console.log("吃饭");
+}
+function fn2() {
+  console.log("工作");
+}
+event.on("我的一天", fn1);
+event.on("你的一天", fn2);
+event.emit("我的一天");
